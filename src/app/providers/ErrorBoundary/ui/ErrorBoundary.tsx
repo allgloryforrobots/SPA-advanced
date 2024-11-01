c4155c0d459d88a27469be2bbd2d27024ad5c581
Илья Булатов <ilia.bulatov@infoguild.ru>
2024-11-01 08:44:30 +0300

    Доработал тесты на кнопку и поправил ошибку ts

--- src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -15,7 +15,7 @@ class ErrorBoundary extends Component<IErrorBoundaryProps, IErrorBoundaryState> 
         this.state = { hasError: false }
     }
 
-    static getDerivedStateFromError(error: Error) {
+    static getDerivedStateFromError(_error: Error) {
         // Update state so the next render will show the fallback UI.
         return { hasError: true }
     }
