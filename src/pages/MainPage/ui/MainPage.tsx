c81d74d2f921a3797ed9025656ba791748530525
Илья Булатов <ilia.bulatov@infoguild.ru>
2024-11-22 10:08:46 +0300

    sidebar: unit tests

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,18 +1,9 @@
 import { useTranslation } from 'react-i18next'
-import { Button } from 'shared/ui/Button/Button'
-import { AppLink } from 'shared/ui/AppLink/AppLink'
 
 const MainPage = () => {
     const { t } = useTranslation('main')
 
     return <div>
-        <Button>
-            {t('Главная страница')}
-        </Button>
-        <AppLink to={'/'}>
-            {t('Главная страница')}
-        </AppLink>
-
         {t('Главная страница')}
     </div>
 }
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,16 +1,16 @@
 import { fireEvent, screen } from '@testing-library/react'
 import { Sidebar } from 'widgets/Sidebar'
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation'
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender'
 
 describe('Sidebar', () => {
     test('Test render', () => {
-        renderWithTranslation(<Sidebar />)
+        componentRender(<Sidebar />)
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
         screen.debug()
     })
 
     test('Test toggle', () => {
-        renderWithTranslation(<Sidebar />)
+        componentRender(<Sidebar />)
         const toggleBtn = screen.getByTestId('sidebar-toggle')
         expect(screen.getByTestId('sidebar-toggle')).toBeInTheDocument()
         fireEvent.click(toggleBtn)
